76713b329f432580ed6b4009babdd7cc94fb503e
Antonio Delgado <adelgado.mx@gmail.com>
2021-06-13 01:29:59 -0500

    Ejercicio de variables, primero del curso

---
+++ python.easy/variables.exc/variables.py
@@ -0,0 +1,26 @@
+'''
+    Simple print varialbes programa / adelgado 2021
+'''
+def Which_Song():   
+    # Define variables
+    Name = "The Duellist"
+    Artist = "Iron Maiden"
+    Genre = "Heavy Metal"
+    YearPublished = 1984
+    Writer = "Harris"
+    Company = "EMI Capitol"
+    SecondsLength = 271
+    Producer = "Martin Birch"
+
+    # Print the variables
+    print(Name)
+    print(Artist)
+    print(Genre)
+    print(YearPublished)
+    print(Writer)
+    print(Company)
+    print(SecondsLength)
+    print(Producer)
+
+# Run de program
+Which_Song()
